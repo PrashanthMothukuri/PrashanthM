aafa2e533ecc20deecd0541c2af7ecb2013fdbc9
PrashanthMothukuri <101607958+PrashanthMothukuri@users.noreply.github.com>
2022-03-16 03:21:58 +0530

    Update Hello.java
    Fixing syntax

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(string[] args) {
     // Print Hello world in the terminal window.
-    System.out.println("Hello, World !!")
+    System.out.println("Hello, World !!");
   }
 }
